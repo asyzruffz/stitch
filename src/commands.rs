19fdaa4210b957ea962e9f318ca780b97538ec93
Asyraf <asyzruffz@gmail.com>
2024-09-02 12:50:54 +0800

    Add dummy commands

---
+++ src/commands.rs
@@ -0,0 +1,16 @@
+
+pub fn create_project(name: &str) {
+
+}
+
+pub fn build_project() {
+    
+}
+
+pub fn run_project() {
+    
+}
+
+pub fn test_project() {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::{self, Write};
+use clap::{Parser, Subcommand};
+
+mod projects;
+mod commands;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    New { project: String },
+    Build,
+    Run,
+    Test,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Some(Commands::New { project }) => commands::create_project(&project),
+        Some(Commands::Build) => commands::build_project(),
+        Some(Commands::Run) => commands::run_project(),
+        Some(Commands::Test) => commands::test_project(),
+
+        None => writeln!(io::stderr(), "Unknown command entered").unwrap(),
+    };
 }
