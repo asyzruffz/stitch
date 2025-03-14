2270ee4cd7b9f5fd89dddaa98d0e13bcb9ba99eb
Asyraf <asyzruffz@gmail.com>
2025-03-14 18:07:43 +0800

    Add condition evaluation

--- src/compilation/intepreter.rs
+++ src/compilation/intepreter.rs
@@ -57,7 +57,7 @@ fn evaluate(phrase : &Phrase, environment: &mut Environment) -> Result<Evaluatio
         Phrase::Postfix { noun, adjective } => todo!(),
         Phrase::Prefix { prefix, noun } => todo!(),
         Phrase::Action { subject, verb, object } => todo!(),
-        Phrase::Condition { left, conjunction, right } => todo!(),
+        Phrase::Condition { left, conjunction, right } => evaluate_condition(conjunction, left, right, environment),
     }
 }
 
@@ -73,6 +73,100 @@ fn evaluate_primitive(primitive: &Primitive, environment: &Environment) -> Resul
             Ok(value.clone())
         } else {
             Err(EvaluationError::new(&format!("Undefined variable \"{}\".", name)))
+        },
+    }
+}
+
+fn evaluate_condition(conjunction: &Conjunction, left : &Phrase, right : &Phrase, environment: &mut Environment) -> Result<Evaluation, EvaluationError> {
+    match conjunction {
+        Conjunction::Greater | Conjunction::GreaterEqual | Conjunction::Less | Conjunction::LessEqual => {
+            let left_phrs = evaluate(left, environment);
+            let right_phrs = evaluate(right, environment);
+
+            let error = if let Err(err) = left_phrs.clone() { Some(err) } else { None };
+            let error = if let Err(err) = right_phrs.clone() { error.and_then(|e| Some(e.concat(err))) } else { error };
+
+            if let (Ok(Evaluation::Number(lvalue)),
+                    Ok(Evaluation::Number(rvalue)))
+                        = (left_phrs, right_phrs) {
+
+                match conjunction {
+                    Conjunction::Greater => Ok(Evaluation::Boolean(lvalue > rvalue)),
+                    Conjunction::GreaterEqual => Ok(Evaluation::Boolean(lvalue >= rvalue)),
+                    Conjunction::Less => Ok(Evaluation::Boolean(lvalue < rvalue)),
+                    Conjunction::LessEqual => Ok(Evaluation::Boolean(lvalue <= rvalue)),
+                    _ => unreachable!(),
+                }
+                
+            } else {
+                Err(EvaluationError::new("Invalid comparison operand not as numbers").concat_if(error))
+            }
+        },
+        Conjunction::Equal | Conjunction::NotEqual => {
+            let left_phrs = evaluate(left, environment);
+            let right_phrs = evaluate(right, environment);
+
+            let error = if let Err(err) = left_phrs.clone() { Some(err) } else { None };
+            let error = if let Err(err) = right_phrs.clone() { error.and_then(|e| Some(e.concat(err))) } else { error };
+
+            if let (Ok(left_result), Ok(right_result)) = (left_phrs, right_phrs) {
+
+                match conjunction {
+                    Conjunction::Equal => Ok(Evaluation::Boolean(Evaluation::equal(&left_result, &right_result))),
+                    Conjunction::NotEqual => Ok(Evaluation::Boolean(!Evaluation::equal(&left_result, &right_result))),
+                    _ => unreachable!(),
+                }
+                
+            } else {
+                Err(EvaluationError::new("Invalid equality operand").concat_if(error))
+            }
+        },
+        Conjunction::And => {
+            let result = evaluate_truth(evaluate(left, environment)?, environment)?;
+            match result {
+                Evaluation::Boolean(value) => {
+                    if !value { Ok(result) }
+                    else {
+                        let result = evaluate_truth(evaluate(right, environment)?, environment)?;
+                        match result {
+                            Evaluation::Boolean(_) => Ok(result),
+                            _ => Err(EvaluationError::new("Invalid logic operand")),
+                        }
+                    }
+                }
+                _ => Err(EvaluationError::new("Invalid logic operand")),
+            }
+        },
+        Conjunction::Or => {
+            let result = evaluate_truth(evaluate(left, environment)?, environment)?;
+            match result {
+                Evaluation::Boolean(value) => {
+                    if value { Ok(result) }
+                    else {
+                        let result = evaluate_truth(evaluate(right, environment)?, environment)?;
+                        match result {
+                            Evaluation::Boolean(_) => Ok(result),
+                            _ => Err(EvaluationError::new("Invalid logic operand")),
+                        }
+                    }
+                }
+                _ => Err(EvaluationError::new("Invalid logic operand")),
+            }
+        },
+        Conjunction::None => Err(EvaluationError::new("None conjunction invalid")),
+    }
+}
+
+fn evaluate_truth(value : Evaluation, environment: &mut Environment) -> Result<Evaluation, EvaluationError> {
+    match value {
+        Evaluation::Void => Err(EvaluationError::new("Invalid boolean condition for void")),
+        Evaluation::Number(_) => Ok(Evaluation::Boolean(true)),
+        Evaluation::Text(_) => Ok(Evaluation::Boolean(true)),
+        Evaluation::Boolean(value) => Ok(Evaluation::Boolean(value)),
+        Evaluation::Custom(var) => if let Some(value) = environment.get(var.as_ref()) {
+            evaluate_truth(value.clone(), environment)
+        } else {
+            Err(EvaluationError::new(&format!("Undefined variable {}.", var.as_ref())))
         },
     }
 }
