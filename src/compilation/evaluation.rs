e88081e4ff7305f2e5c7c284d9bd8a521dcc5cb0
Asyraf <asyzruffz@gmail.com>
2025-10-06 09:23:03 +0800

    Handle parity for noun

--- src/compilation/evaluation.rs
+++ src/compilation/evaluation.rs
@@ -84,31 +84,40 @@ impl Evaluation {
                 None => Ok(()),
                 Some(datatype) => Err(format!("expected {} but found Void", datatype).into()),
             },
-            (Evaluation::Number(_), Evaluation::Number(_)) => Ok(()),
-            (Evaluation::Text(_), Evaluation::Text(_)) => Ok(()),
-            (Evaluation::Boolean(_), Evaluation::Boolean(_)) => Ok(()),
             (Evaluation::Collective(expected), Evaluation::Collective(found)) => {
-                if expected.len() == found.len() && expected.iter().zip(found.iter()).all(|(e1, e2)| e1.parity(e2).is_ok()) {
-                    Ok(())
-                } else {
-                    Err("collectives do not match in length or type".into())
+                if expected.len() != found.len() {
+                    return Err(format!("collectives do not match in length, expected {} but found {} object(s)", expected.len(), found.len()).into());
                 }
+
+                expected.iter()
+                        .zip(found.iter())
+                        .map(|(e1, e2)| e1.parity(e2))
+                        .collect::<Result<Vec<_>, Rc<str>>>()
+                        .map(|_| ())
             },
             (Evaluation::Collective(expected), found) => {
                 if expected.len() == 1 {
                     expected[0].parity(found)
                 } else {
-                    Err(format!("expected collective of {} but found single object", expected.len()).into())
+                    Err(format!("collectives do not match in length, expected {} but found 1 object(s)", expected.len()).into())
                 }
             },
             (expected, Evaluation::Collective(found)) => {
                 if found.len() == 1 {
                     expected.parity(&found[0])
                 } else {
-                    Err(format!("expected single object but found collective of {}", found.len()).into())
+                    Err(format!("collectives do not match in length, expected 1 but found {} object(s)", found.len()).into())
                 }
             },
-            (expected, found) => { //TODO: Handle Noun, Action, Adjective
+            (Evaluation::Number(_), Evaluation::Number(_)) => Ok(()),
+            (Evaluation::Text(_), Evaluation::Text(_)) => Ok(()),
+            (Evaluation::Boolean(_), Evaluation::Boolean(_)) => Ok(()),
+            (Evaluation::Noun(expected), Evaluation::Noun(found)) => if expected.name.as_ref() == found.name.as_ref() {
+                Ok(())
+            } else {
+                Err(format!("expected {} but found {}", expected.name, found.name).into())
+            },
+            (expected, found) => { //TODO: Handle parity for Action, Adjective
                 let expected_type = expected.datatype().map_or("Void".into(), |dt| format!("{dt}"));
                 let found_type = found.datatype().map_or("Void".into(), |dt| format!("{dt}"));
                 Err(format!("expected {} but found {}", expected_type, found_type).into())
